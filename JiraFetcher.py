365211501e40a914242e19ae790a11041561cefb
Christos Tsotskas <christos@pure-escapes.com>
2020-04-17 10:40:18 +0100

    first commit

---
+++ JiraFetcher.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Copyright (C) Pure Escapes 2020 - All Rights Reserved
+Unauthorized copying of this file, via any medium is strictly prohibited
+Proprietary and confidential
+
+Written by Christos Tsotskas <info@pure-escapes.com>, April 2020
+
+"""
+
+def hello():
+    print('hi jira')
+
+if __name__ == "__main__":
+    hello()
