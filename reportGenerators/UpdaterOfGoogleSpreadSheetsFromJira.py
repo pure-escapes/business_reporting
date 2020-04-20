aeffc91d267c6a2eee436e946a5f608580a4c347
Christos Tsotskas <christos@pure-escapes.com>
2020-04-20 21:29:24 +0100

    calculated correctly the agile estimations

--- reportGenerators/UpdaterOfGoogleSpreadSheetsFromJira.py
+++ reportGenerators/UpdaterOfGoogleSpreadSheetsFromJira.py
@@ -10,13 +10,71 @@ Written by Christos Tsotskas <info@pure-escapes.com>, April 2020
 
 """
 
+import json
+
 class UpdaterOfGoogleSpreadSheetsFromJira:
     __output = {}
+    __configuration = None
+    __additional_files_to_consider = []
 
 
-    def __init__(self):
-        pass
+    def __init__(self, configuration, additional_files_to_consider=[]):
+        self.__configuration = configuration
+        self.__additional_files_to_consider = additional_files_to_consider
+
+    def calculate_team_velocity(self):
+        velocity_of_this_period = self.__configuration["input"]["at_the_end_of_the_period"]["tickets_completed"]
+        number_additional_files = len(self.__additional_files_to_consider)
+        if number_additional_files == 0:
+            self.__configuration["calculated"]["team_velocity"] = velocity_of_this_period
+        else:
+            temp_velocity_from_additional_files = 0
+            for configuration in self.__additional_files_to_consider:
+                with open(configuration, 'r') as input_file:
+                    temp_configuration = json.load(input_file)
+                    temp_velocity_from_additional_files += temp_configuration["input"]["at_the_end_of_the_period"]["tickets_completed"]
+
+            calculated_velocity = (temp_velocity_from_additional_files + velocity_of_this_period)/number_additional_files
+            self.__configuration["calculated"].update( team_velocity = calculated_velocity )
+            # self.__configuration["calculated"]["team_velocity"] = (temp_velocity_from_additional_files + velocity_of_this_period)/number_additional_files
+
+
+
+    def calculate_estimation_to_completion(self):
+        C3 = self.__configuration["input"]["at_the_start_of_the_period"]["backlog_size"]
+        C7 = self.__configuration["input"]["at_the_end_of_the_period"]["tickets_added_to_backlog_during_the_period"]
+        C8 = self.__configuration["input"]["at_the_end_of_the_period"]["bugs_added_to_backlog_during_the_period"]
+
+        C5 = self.__configuration["input"]["at_the_end_of_the_period"]["tickets_completed"]
+        C9 = self.__configuration["input"]["at_the_end_of_the_period"]["tickets_removed_from_backlog_during_the_period"]
+
+        self.__configuration["calculated"].update( estimated_days_to_complete = ( (C3+C7+C8) - (C5+C9) ) / self.__configuration["calculated"]["team_velocity"] )
+
+
+
+
+    def calculate_weighted_iterations_to_completion(self):
+        result = (self.__configuration["calculated"]["estimated_days_to_complete"] *100) / (100 - self.__configuration["input"]["at_the_start_of_the_period"]["capacity_ratio_for_bugs"] )
+        result = round(result, 2)
+        self.__configuration["calculated"].update( weighted_iterations_to_complete=result)
+
+
+    def calculate_relative_difference(self):
+        A = self.__configuration["calculated"]["estimated_days_to_complete"]
+        B = self.__configuration["calculated"]["weighted_iterations_to_complete"]
+        result = ((B - A) * 100) / A
+        result = round (result, 2)
+        self.__configuration["calculated"].update(relative_difference_between_estimation_and_weighted_iterations=result )
+
 
     def update(self):
         status = False
         return status
+
+    def calculate(self):
+        self.calculate_team_velocity()
+        self.calculate_estimation_to_completion()
+        self.calculate_weighted_iterations_to_completion()
+        self.calculate_relative_difference()
+
+        return self.__configuration["calculated"]
--- test/test_Updater.py
+++ test/test_Updater.py
@@ -12,17 +12,55 @@ Written by Christos Tsotskas <info@pure-escapes.com>, April 2020
 
 import unittest
 import os
+import json
 
 from reportGenerators.UpdaterOfGoogleSpreadSheetsFromJira import UpdaterOfGoogleSpreadSheetsFromJira
 
 class Test_JIRAFetcher(unittest.TestCase):
 
-    def test_get_bugs_for_a_specific_version(self):
-        status = False
+    def get_filepath_from_resouces_starting_from_tests_directory(self, target_filename):
+        here = __file__
+        path_to_tests, this_file = os.path.split(here)
+        path_to_root, dummy = os.path.split(path_to_tests)
+        path_to_target_file = os.path.join(path_to_root, 'resources', target_filename)
 
-        u532 = UpdaterOfGoogleSpreadSheetsFromJira()
+        return path_to_target_file
+
+
+    def test_calculations_from_a_single_file(self):
+        status = False
+        configuration = None
+
+        path_to_target_file = self.get_filepath_from_resouces_starting_from_tests_directory('basefile_of_UpdaterConfiguration.json')
+        with open(path_to_target_file, "r") as read_file:
+            configuration = json.load(read_file)
+
+            configuration["input"]["at_the_start_of_the_period"]["backlog_size"] = 68
+            configuration["input"]["at_the_start_of_the_period"]["tickets_committed"] = 10
+            configuration["input"]["at_the_start_of_the_period"]["capacity_ratio_for_bugs"] = 6
+
+            configuration["input"]["at_the_end_of_the_period"]["tickets_completed"] = 5
+            configuration["input"]["at_the_end_of_the_period"]["tickets_added_to_backlog_during_the_period"] = 1
+            configuration["input"]["at_the_end_of_the_period"]["bugs_added_to_backlog_during_the_period"] = 2
+            configuration["input"]["at_the_end_of_the_period"]["tickets_removed_from_backlog_during_the_period"] = 3
+
+
+            configuration["google_settings"]["path_to_configuration_file_to_google_spreadsheet"] = self.get_filepath_from_resouces_starting_from_tests_directory("simple_reports_configuration.json")
+
+
+        u532 = UpdaterOfGoogleSpreadSheetsFromJira(configuration)
 
         status = u532.update()
 
+        output = u532.calculate()
 
-        self.assertTrue(status)
+        self.assertEqual(output["team_velocity"], 5)
+        self.assertAlmostEqual(output["estimated_days_to_complete"], 12.6)
+        self.assertAlmostEqual(output["weighted_iterations_to_complete"], 13.4 )
+        self.assertAlmostEqual(output["relative_difference_between_estimation_and_weighted_iterations"], 6.35)
+
+
+
+
+        # self.assertTrue(status)
+        self.assertTrue(configuration!=None)
